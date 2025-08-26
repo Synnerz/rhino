caf2c6f456d4e0b9cdc6bd70105d0052b3189a5e
Doc <65479309+DocilElm@users.noreply.github.com>
2025-08-26 14:09:49 -0700

    improve call0 call site slightly

--- src/main/java/org/mozilla/javascript/optimizer/LRUCache.java
+++ src/main/java/org/mozilla/javascript/optimizer/LRUCache.java
@@ -5,18 +5,18 @@ import java.util.LinkedHashMap;
 
 public class LRUCache {
     private int capacity;
-    private LinkedHashMap<Integer, OptRuntime.LookupCache> map;
+    private LinkedHashMap<Integer, OptRuntime.NameIC> map;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
         this.map = new LinkedHashMap<>(16, 0.75f, true);
     }
 
-    public OptRuntime.LookupCache get(int key) {
+    public OptRuntime.NameIC get(int key) {
         return this.map.get(key);
     }
 
-    public void put(int key, OptRuntime.LookupCache value) {
+    public void put(int key, OptRuntime.NameIC value) {
         if (!this.map.containsKey(key) && this.map.size() == this.capacity) {
             Iterator<Integer> it = this.map.keySet().iterator();
             it.next();
